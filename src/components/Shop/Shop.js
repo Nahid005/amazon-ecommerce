91c16682ad34b68c7d5ce9d3aa5b94f0b28ee5c3
Nahid005 <nahidhassan9258@gmail.com>
2022-02-04 13:44:54 +0600

    shop page design

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import './Shop.css'
+import { useState } from 'react';
+import data from '../../fakeData/products.json';
+import Product from '../Product/Product';
+import './Shop.css';
 
 const Shop = () => {
-    
+    const addCart = (product)=> {
+        console.log('product added', product);
+    }
+    const frist10 = data.slice(0, 10);
+    const [products, setProducts] = useState(frist10) ;
+    const [cart, setCart] = useState([]);
     return (
-        <div>
-            
+        <div className='shop-container'>
+            <div className='product-card'>
+                {
+                    products.map(pd => <Product addcart={addCart} product={pd}></Product>)
+                }
+            </div>
+            <div className='cart-info'>
+
+            </div>
         </div>
     );
 };
